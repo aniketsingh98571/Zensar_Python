cd20deee304eedeb77f8f00eaeec1cb9992c0bfb
aniketsingh98571 <thor98571@gmail.com>
2021-05-10 18:50:53 +0530

    Day1.py

--- Day1.py
+++ Day1.py
@@ -55,3 +55,84 @@ Set
 Dictiionary
 tuple
 '''
+
+#arithmatic operator:- +,-,/,**,//
+m=5
+n=6
+print(m)
+print(n)
+print("Addition + ",m+n)
+print("Substraction - ",m-n)
+print("Multiplication * ",m*n)
+print(" Division /", m/2)
+print("Floor division //", n//2)#quotient
+print("Floor division ", m//5)
+print(" The exponential ** ", 2**2)
+print("Remainder %",n%2)
+
+#Comparison operator(==,!=,<,>,<=,>=)
+k=2
+l=6
+print(k)
+print(l)
+print(k==l)
+
+print(k!=12)
+print("Less than",k<l)
+print(" greater than ",k>l)
+print(" less than =",k<=l)
+print("greater than =",k>=l)
+
+#Shorthand operator
+o=5
+i=6
+o+=i #o=o+i
+print("Addition is ",o)
+
+i-=o #i=i-o
+print("Substraction ", i)
+
+o*=5 #o=o*5
+print("Multiplication ",o)
+
+i/=3 #i=i/3
+print(" Division ",i)
+#Bitwise operator
+#Logical operator
+#membership operator-in, not in
+#Logical operator- is,is not
+
+#write a program to demonstrate diff data types in python, print variable values 
+#with types
+integer_1=5
+print(integer_1)
+print(type(integer_1))
+
+float_1=15.6
+print(float_1)
+print(type(float_1))
+
+String_1="Aniket"
+print(String_1)
+print(type(String_1))
+
+Boolean_1=True
+print(type(Boolean_1))
+
+#Write a program to demonstrate arithmatic operators
+t=5
+y=6
+print(" Addition",t+y)
+print("Substraction ",t-y)
+print("Multiplication ",t*y)
+print(" Division ",t/y)
+
+#Write a program to demonstrate comparison operator
+v=5
+r=6
+print(" greater",t>y)
+print("less ",t<y)
+print("Equal" ,t==y)
+print(" greater than = ",t>=y)
+print("less than =",t<=y)
+
